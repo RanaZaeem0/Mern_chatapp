5b6b7703a7f36fcd67bac12a10864720b032de25
RanaZaeem0 <ranazaeem34@gmail.com>
2024-11-24 11:29:10 +0500

    cors doamin update

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,9 @@ app.use(cookieParser())
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "http://localhost:4173",
+        "http://localhost:5174",
+        "http://localhost:5175",
+
       ],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
